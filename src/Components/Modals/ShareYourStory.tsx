f45d22c591c41546ea8cba871dc826a24c56bd99
George Crisan <georgerdp@gmail.com>
2020-10-11 15:50:32 +0100

    [style]: Improve story form

--- src/Components/Modals/ShareYourStory.tsx
+++ src/Components/Modals/ShareYourStory.tsx
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import {FormControl, InputLabel, Select, TextField, MenuItem, Button, } from '@material-ui/core';
-import { css } from "emotion";
-//import formImage from '../../images/story-form.jpg';
+import {FormControl, InputLabel, Select, TextField, MenuItem, Button, Paper} from '@material-ui/core';
+import { css , cx} from "emotion";
+import formImage from '../../images/story-form.jpg';
+import formImage2 from '../../images/story-form2.jpg';
 import ButtonCustom from '../ButtonCustom';
 
 interface FormState  {
@@ -25,8 +26,21 @@ function ShareYourStory() {
   });
 
   return (
+    <>
+      <div className={cx(banner,banner2)} >
 
-    <div className={formWrapper}>
+        <div className={insideBanner}>
+          <p className={mainP} >
+            Tell the world how you feel, share your thoughts, be yourself. </p>
+          <p className={secP} > 
+            You do not have to provide personal or sensitive
+            informations if you don't want to.
+          </p>
+          <p className={secP}  > All the stories are moderated before are being listed publicly. 
+          </p>
+        </div>
+      </div>
+      <Paper variant='outlined' className={formWrapper}>
         <FormControl style={{padding: 16, width: "100%", boxSizing: 'border-box'}}   variant="outlined">
           <TextField  id="outlined-basic" value={formState.title} label="Story Title" variant="outlined" />
         </FormControl>
@@ -67,17 +81,65 @@ function ShareYourStory() {
         <Button style={{height: 46, marginBottom: 16, minWidth: 200}} color='secondary' variant='outlined'> Cancel </Button>
         <ButtonCustom onClick={(event)=> null } variant='outlined' size='large' color='default' > Submit your story </ButtonCustom>
       </div>
-    </div>
-
+    </Paper>
+    <div className={banner} style={{marginBottom: 16}}>
+        <p className={mainP}   >
+          Sometimes it helps to know how other feel and sometimes is nice to be able to express yourself.</p>
+        <p className={secP} > 
+          You do not have to provide personal or sensitive
+          informations if you don't want to.
+        </p>
+        <p className={secP} style={{marginBottom: 16}}  > All the stories are moderated before are being listed publicly. 
+        </p>
+      </div>
+    </>
   );
 } 
 
 export default ShareYourStory;
 
+const insideBanner = css`
+  margin: auto;
+  width: 90%;
+`;
+
+const banner = css`
+  max-width: 800px;
+  margin: auto;
+  height: 280px;
+  padding: 16px;
+  background-image: url(${formImage});
+  background-size: cover;
+  background-position: 50%;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 32px;
+  margin-bottom: 32px;
+  box-sizing: border-box; 
+  position: relative;
+    z-index: 1;
+    &:after {
+      display: block;
+      content: ' ';
+      position: absolute;
+      top: 0;
+      height: 100%;
+      width: 100%;
+      background: rgba(0,30,30,0.5);
+    }
+`;
+
+const banner2 = css`
+  background-image: url(${formImage2});
+`;
+
 const formWrapper = css`
   margin: 16px auto;
   box-sizing: border-box;
   background: #f8f8f8;
+  border-radius: 5px;
   width: 100%;
   max-width: 800px;
 `;
@@ -102,4 +164,28 @@ const formButtons = css`
   @media (min-width: 540px) {
     flex-flow: row nowrap;
   }
+`;
+
+
+const mainP = css`
+  color: #fff;
+  padding: 0 0 16px 0;
+  font-size: 20px;
+  text-align: center;
+  font-family: "Fira Sans", sans-serif;
+  text-shadow: 1px 0px 0px #555;
+  position: relative;
+  z-index: 5;
+`;
+
+const secP = css`
+color: #fff;
+padding: 16px 0 0 0;
+margin: 0 0 0 0;
+font-size: 14px;
+text-align: center;
+font-family: "Fira Sans", sans-serif;
+textShadow: 1px 0px 0px #555;
+position: relative;
+z-index: 5;
 `;
