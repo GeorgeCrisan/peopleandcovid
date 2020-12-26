fd27ceebec376176be6ba623b7d944b1d91f2900
George Crisan <georgerdp@gmail.com>
2020-12-26 12:54:29 +0000

    [FEATURE] Finish footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import {css} from 'emotion';
+import { css } from 'emotion';
 import { grey } from '@material-ui/core/colors';
 
-const Footer:React.FC = () => {
+const Footer: React.FC = () => {
 
 
   return (<div className={footerAll} >
-      <div className={footerContainer}>
-        <div>
-          <h4 style={{fontFamily: "'Fira Sans', sans-serif"}} >Useful links</h4>
-          <ul className={listStyle}>
-             <li> Items </li>
-           </ul>
+    <div className={footerContainer}>
+      <div>
+        <h4 style={{ fontFamily: "'Fira Sans', sans-serif", color: 'rgba(3, 168, 124, 1)' }} >Useful Links</h4>
+        <ul className={listStyle}>
+          <a href='https://www.who.int/' ><li> World Health Organization </li></a>
+          <a href='https://www.gov.uk/coronavirus' ><li> UK GOV Coronavirus </li></a>
+          <a href='https://www.who.int/emergencies/diseases/novel-coronavirus-2019/covid-19-vaccines'><li> Covid-19 Vaccines </li></a>
+          <a href='https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public'><li> Advice for the public </li></a>
+        </ul>
 
-        </div>
-        <div>
-           <h4 style={{fontFamily: "'Fira Sans', sans-serif"}} > More info more links </h4>
-           <ul className={listStyle}>
-             <li> Items </li>
-           </ul>
-        </div>
       </div>
-    </div>);
+      <div>
+        <h4 style={{ fontFamily: "'Fira Sans', sans-serif", color: 'rgba(3, 168, 124, 1)' }} > Reports and Statistics </h4>
+        <ul className={listStyle}>
+          <a href='https://www.who.int/data/gho' ><li> Global Health Observatory </li></a>
+          <a href='https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports' > <li> Reports </li>  </a>
+          <a href="https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases"> <li> ECDC Europe </li></a>
+          <a href="https://coronavirus.data.gov.uk/"> <li> UK Daily Update </li></a>
+        </ul>
+      </div>
+    </div>
+  </div>);
 
 };
 
@@ -43,6 +49,22 @@ const footerAll = css`
 
 const footerContainer = css`
   margin-top: 16px;
+  padding: 16px;
+  a {
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  li {
+    border-bottom: 1px solid white;
+    margin-top: 16px;
+    max-width: 90%;
+    &: hover {
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
   width: 100%;
   max-width: 800px;
   display: flex;
